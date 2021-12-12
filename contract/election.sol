794c66511f9d092ae952820ee81b0ac28de3ad6e
nisamozr <codeexplorkl10@gmail.com>
2021-12-12 14:29:52 +0530

    code standad

--- contract/election.sol
+++ contract/election.sol
@@ -1,48 +1,44 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity 0.8.7;
 
-contract voterId{
-
- struct VoterIdcard{
+contract VoterId{
+ struct voterIdcard{
         string voterName;
         uint voterWard;
         uint age;
         uint voterIds;
-     
     }
     uint num = 1000;
     uint count;
+    mapping (uint => voterIdcard) public voteridcard;
 
-     mapping (uint => VoterIdcard) public voteridcard;
-     function voterIdReg(string memory _name, uint _ward, uint age ) public{
-     
-         count++;
-           voteridcard[count] = VoterIdcard(_name,_ward,age, num+count );
-
-     }
+    function voterIdReg(
+        string memory _name,
+        uint _ward,
+        uint _age 
+    ) 
+        public
+    {
+        require(
+            _age >= 18,
+            "This person is not eligible for voters Id"
+        );
+        count++;
+        voteridcard[count] = voterIdcard(
+            _name,
+            _ward,
+            _age,
+            num+count
+        );
+    }
 }
 
-
-contract election is voterId{
-    address owner;
+contract Election is VoterId{
+    address authority;
     constructor(){
-        owner = msg.sender;
+        authority = msg.sender;
     }
-      enum candidateStatus {pending, approved, rejected}
-    
-    struct  Candidats{
-        uint CandidateId;
-        string name;
-        uint ward;
-        uint voteCount;
-        address id;
-        uint VoterId;
-        candidateStatus  status;
-      
-        
-    } 
-    struct Voter{
+     struct Voter{
         uint index;
         string voterName;
         uint voterWard;
@@ -50,125 +46,131 @@ contract election is voterId{
         bool verify;
         uint voterId;
     }
-
-    
+    struct  Candidates{
+        uint CandidateId;
+        string name;
+        uint ward;
+        uint voteCount;
+        address id;
+        uint VoterId;
+        candidateStatus  status;   
+    } 
+    enum candidateStatus {pending, approved, rejected}
     mapping(address => bool) public votes;
     mapping (uint => Voter) public voter;
-    mapping (uint  => Candidats) public candidateReg;
-    mapping (uint  => Candidats) public candidatslist;
-    uint candedatsCount;
+    mapping (uint  => Candidates) public candidateReg;
+    mapping (uint  => Candidates) public candidateslist;
+    uint candedatesCount;
     uint voterCount;
     uint approvedCandidateCount;
  
-    
-    modifier onlyOwner(){
-        require(msg.sender == owner,"Only Owner can add candidate");
+    modifier authorization(){
+        require(msg.sender == authority,"authority can only call this function");
         _;
     }
     
-    function voterRegistration( string memory _name, uint _ward, uint _voterId ) public {
-        for(uint i=1; i<= voterCount; i++ ){
+    function voterRegistration(
+        string memory _name, 
+        uint _ward, 
+        uint _voterId 
+    ) 
+        public
+    {
+        for(uint i = 1; i <= voterCount; i++ ){
             if(voter[i].voterAddress == msg.sender){
                 revert("already created");
             }
         }
         voterCount++;
-        voter[voterCount] = Voter(voterCount ,_name, _ward, msg.sender, false, _voterId);
+        voter[voterCount] = Voter(
+            voterCount,
+            _name,
+            _ward,
+            msg.sender,
+            false,
+            _voterId
+        );
     }
 
-     function voterVerify(uint _voterId) public onlyOwner {
-        
+    function voterVerify(uint _voterId) public authorization {
             if(voter[_voterId].verify == true){
                 revert("already verify");
             }
             else if(_voterId < 0 || _voterId > count  ){
                 revert("voter not found");
-            }
-               
-             else if(voter[_voterId].voterId == voteridcard[_voterId].voterIds){
+            }   
+            else if(voter[_voterId].voterId == voteridcard[_voterId].voterIds){
                   voter[_voterId].verify = true;
             }else{
                  revert("voter id no found");
-
             }       
     }
+
     function applyForCantidate()public{
-     
-       
-     
-         for(uint i=1; i<= voterCount; i++ ){
-            //   require(voter[i].voterAddress == msg.sender, "account  not match");
-           
-                  if(voter[i].voterAddress == msg.sender && voter[i].verify == true){
-               
-                    candedatsCount++;
+         for(uint i=1; i<= voterCount; i++ ){           
+                if(voter[i].voterAddress == msg.sender && voter[i].verify == true){
+                    candedatesCount++;
                     candidateStatus _candidateStatus = candidateStatus.pending;
-                    candidateReg[candedatsCount] = Candidats(candedatsCount, voter[i].voterName,voter[i].voterWard,0,voter[i].voterAddress, voter[i].voterId, _candidateStatus);
+                    candidateReg[candedatesCount] = Candidates(
+                        candedatesCount,
+                        voter[i].voterName,
+                        voter[i].voterWard,
+                        0,
+                        voter[i].voterAddress, 
+                        voter[i].voterId, 
+                        _candidateStatus
+                    );
 
-           
-            }else if(candidateReg[i].id == msg.sender || voterCount == 0){
-                   revert("same candidate can't be addd");
-            }
-
-             
-               
-                  
-        }
-        
+                }
+                else if(candidateReg[i].id == msg.sender || voterCount == 0){
+                    revert("same candidate can't be addd");
+                }          
+        } 
     }
 
-     function approveCadidedress(address _voteraddress) public onlyOwner {
+    function approveCadidedress(address _voteraddress) public authorization {
          for(uint j ; j<=approvedCandidateCount;j++){
-             if(candidatslist[j].id == _voteraddress){
+             if(candidateslist[j].id == _voteraddress){
                  revert("already aproved");
              }
          }
-            for(uint i=1; i<= candedatsCount; i++ ){
-          
-                if(candidateReg[i].id == _voteraddress){
-                    for(uint j =1; j<= count; j++){
+        for(uint i=1; i<= candedatesCount; i++ ){
+            if(candidateReg[i].id == _voteraddress){
+                for(uint j =1; j<= count; j++){
                     if( candidateReg[i].ward == voteridcard[j].voterWard && candidateReg[i].VoterId == voteridcard[j].voterIds){
-                                
-                                approvedCandidateCount++;
-                                candidateStatus _candidateStatus = candidateStatus.approved;
-                                 candidateReg[i] = Candidats(candedatsCount, candidateReg[i].name,candidateReg[i].ward,0,msg.sender, candidateReg[i].VoterId, _candidateStatus);
-                                candidatslist[approvedCandidateCount] = Candidats(approvedCandidateCount, candidateReg[i].name,candidateReg[i].ward,0,_voteraddress, candidateReg[i].VoterId, _candidateStatus);    
-                      }
-                    //   else{
-                    //        candidateStatus _candidateStatus = candidateStatus.rejected;
-                    //        candidateReg[i] = Candidats(candedatsCount, voter[i].voterName,voter[i].voterWard,0,msg.sender, voter[i].voterId, _candidateStatus);
-
-                    //     }
-                                }
-
-
-                }else if(candidatslist[i].id == _voteraddress ){
-                    revert("alreay approved");
+                        approvedCandidateCount++;
+                        candidateStatus _candidateStatus = candidateStatus.approved;
+                        candidateReg[i] = Candidates(candedatesCount, candidateReg[i].name,candidateReg[i].ward,0,msg.sender, candidateReg[i].VoterId, _candidateStatus);
+                        candidateslist[approvedCandidateCount] = Candidates(
+                            approvedCandidateCount, 
+                            candidateReg[i].name,
+                            candidateReg[i].ward,
+                            0,
+                            _voteraddress, 
+                            candidateReg[i].VoterId, 
+                            _candidateStatus
+                        );    
+                    }
                 }
-
             }
+        }
      }
-
 
      function result() public view returns(string memory , uint ){
          require(approvedCandidateCount>=1, "there is no candidate");
          uint votecont ;
          uint index ;
          for(uint i =1 ; i<=approvedCandidateCount; i++){
-             if(candidatslist[i].voteCount > votecont){
-                 votecont = candidatslist[i].voteCount;
+             if(candidateslist[i].voteCount > votecont){
+                 votecont = candidateslist[i].voteCount;
                  index = i;
              }
-
          }
-        return(candidatslist[index].name,candidatslist[index].voteCount);
-
-
-         
-     }
-    
- 
-     
+        return(
+            candidateslist[index].name,
+            candidateslist[index].voteCount
+        );   
+     } 
 }
 
 
--- contract/votingSystem.sol
+++ contract/votingSystem.sol
@@ -1,27 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity 0.8.7;
 
 import "./election.sol";
 
-contract voteing is election{
-
-     function vote(uint candidatsId) public {
-
-        require(candidatsId > 0 && candidatsId <=  candedatsCount, "invalide user");
+contract Voteing is Election{
+     function vote(uint candidatesId) public {
+        require(candidatesId > 0 && candidatesId <=  candedatesCount, "invalide user");
         require(!votes[msg.sender], "you are already voted");
         
         for(uint i=1; i<=voterCount; i++ ){
             if(voter[i].voterAddress == msg.sender){
-                if(voter[i].voterWard == candidatslist[candidatsId].ward && voter[i].verify == true){
+                if(voter[i].voterWard == candidateslist[candidatesId].ward && voter[i].verify == true){
                      votes[msg.sender] = true;
-                      candidatslist[candidatsId].voteCount++;
+                      candidateslist[candidatesId].voteCount++;
                 }else{
                     revert("you can't vote another ward candidate");
                 }       
             }
         }
     }
-
-
 }
